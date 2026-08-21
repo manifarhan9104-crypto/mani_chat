d2f15e82fe4afdab8e9d1f15cff81e96199f33d2
manifarhan9104-crypto <manifarhan9104@gmail.com>
2026-08-21 13:37:00 +0330

    Update main.dart

--- mobile/mobile/lib/main.dart
+++ mobile/mobile/lib/main.dart
@@ -1,132 +1,52 @@
 import 'package:flutter/material.dart';
 
+import 'screens/login.dart';
+
+
 void main() {
-  runApp(const ManiChat());
+
+  runApp(
+    const ManiChat()
+  );
+
 }
 
-class ManiChat extends StatelessWidget {
+
+class ManiChat
+    extends StatelessWidget {
+
   const ManiChat({super.key});
 
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(
+      BuildContext context
+  ) {
+
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "مانی چت",
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const ChatPage(),
-    );
-  }
-}
 
+      debugShowCheckedModeBanner:
+          false,
 
-class ChatPage extends StatefulWidget {
-  const ChatPage({super.key});
+      title:
+          "مانی چت",
 
-  @override
-  State<ChatPage> createState() => _ChatPageState();
-}
+      theme:
+          ThemeData(
 
+        useMaterial3:
+            true,
 
-class _ChatPageState extends State<ChatPage> {
-
-  final TextEditingController controller =
-      TextEditingController();
-
-  List<String> messages = [];
-
-
-  void sendMessage(){
-
-    if(controller.text.isNotEmpty){
-
-      setState(() {
-        messages.add(controller.text);
-      });
-
-      controller.clear();
-    }
-
-  }
-
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Directionality(
-
-      textDirection: TextDirection.rtl,
-
-      child: Scaffold(
-
-        appBar: AppBar(
-          title: const Text("پیام‌رسان مانی"),
-        ),
-
-
-        body: Column(
-
-          children: [
-
-            Expanded(
-
-              child: ListView.builder(
-
-                itemCount: messages.length,
-
-                itemBuilder: (context,index){
-
-                  return ListTile(
-                    title: Text(messages[index]),
-                  );
-
-                },
-
-              ),
-
-            ),
-
-
-            Row(
-
-              children: [
-
-                Expanded(
-
-                  child: TextField(
-
-                    controller: controller,
-
-                    decoration:
-                    const InputDecoration(
-                      hintText: "پیام خود را بنویسید..."
-                    ),
-
-                  ),
-
-                ),
-
-
-                IconButton(
-
-                  icon: const Icon(Icons.send),
-
-                  onPressed: sendMessage,
-
-                )
-
-              ],
-
-            )
-
-          ],
-
-        ),
+        colorSchemeSeed:
+            Colors.blue,
 
       ),
+
+      home:
+          const LoginPage(),
 
     );
 
   }
+
 }
